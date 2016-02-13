828807424b214d992910a012c7c62f774dc3138f
Christopher Banh <chrisngobanh@gmail.com>
2016-02-13 15:20:11 -0800

    Initial work on Facebook integration

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-var express = require('express');
+var bodyParser = require('body-parser');
 var cfenv = require('cfenv');
+var express = require('express');
 var request = require('request');
 
 var app = express();
 var appEnv = cfenv.getAppEnv();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(appEnv.port);
 
-console.log('Server running on port 4000');
+console.log('Server running on ', appEnv.url);
 
 app.get('/', function(req, res) {
-//	request.post('https://graph.facebook.com/oauth/device?type=device_code&client_id=523614004477797&scope=public_profile,user_friends,email', function(err, res) {
-//		console.log(res.body);
-//	});
-	res.send('Hello World!');
+  res.redirect('https://www.facebook.com/dialog/oauth?client_id=523614004477797&redirect_uri=http://procrastinationation.mybluemix.net/facebook');
+});
+
+app.post('/facebook', function(req, res) {
+  console.log(req.body);
+    /*request.post('https://www.facebook.com/dialog/oauth?client_id=523614004477797&redirect_uri=http://procrastinationation.mybluemix.net/facebook', function(err, res) {
+      console.log(res.body);
+    });*/
 });
