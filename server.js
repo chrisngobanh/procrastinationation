f046f833075c8145375c330371b6d3e45cbaedd8
Christopher Banh <chrisngobanh@gmail.com>
2016-02-13 12:46:33 -0800

    TEMP: Change GET '/' to send "Hello World"

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ app.listen(4000);
 console.log('Server running on port 4000');
 
 app.get('/', function(req, res) {
-	request.post('https://graph.facebook.com/oauth/device?type=device_code&client_id=523614004477797&scope=public_profile,user_friends,email', function(err, res) {
-		console.log(res.body);
-	});
+//	request.post('https://graph.facebook.com/oauth/device?type=device_code&client_id=523614004477797&scope=public_profile,user_friends,email', function(err, res) {
+//		console.log(res.body);
+//	});
+	res.send('Hello World!');
 });
