91741637cf3b4579f25149a8a649ec5de057f48f
Christopher Banh <chrisngobanh@gmail.com>
2016-02-13 18:38:10 -0800

    Initial implementation of Cloudant

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var bodyParser = require('body-parser');
 var cfenv = require('cfenv');
+var Cloudant = require('cloudant');
 var express = require('express');
 var request = require('request');
 
@@ -15,17 +16,25 @@ app.listen(appEnv.port);
 
 console.log('Server running on', appEnv.url);
 
+var cloudant = Cloudant('https://631d8429-f7da-49a0-91c2-a6b295a5ef0a-bluemix:94418a16ff22256659b0071300260597e371eeacb60689b4ddb95a2f9249c631@631d8429-f7da-49a0-91c2-a6b295a5ef0a-bluemix.cloudant.com');
+
+cloudant.db.list(function(err, allDbs) {
+  console.log('All my databases: %s', allDbs.join(', '))
+});
+
+var users = cloudant.db.use('users')
+
 app.get('/facebook', function(req, res) {
   var code = req.query.code;
 
 
   baseRequest.post('https://graph.facebook.com/v2.3/oauth/access_token?client_id=523614004477797&redirect_uri=http://procrastinationation.mybluemix.net/facebook&client_secret=9fb99aa844667e815c990b41aa086d27&code=' + code, function(err, res1) {
     var access_token = res1.body.access_token;
+    console.log(access_token);
     // Validate the token and its permissions
     baseRequest.get('https://graph.facebook.com/me/permissions?access_token=' + access_token, function(err, res2) {
       var permissions = res2.body.data;
 
-      console.log(res2.body);
       var isGood = false;
 
       var userFriends = 'user_friends';
@@ -49,3 +58,4 @@ app.get('/facebook', function(req, res) {
     });
   });
 });
+
