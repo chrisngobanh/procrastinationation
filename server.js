dded7d41ab4922a7e81442f844f923201326d98e
Christopher Banh <chrisngobanh@gmail.com>
2016-02-13 16:45:42 -0800

    Check if user token if valid

--- server.js
+++ server.js
@@ -16,10 +16,34 @@ console.log('Server running on', appEnv.url);
 app.get('/facebook', function(req, res) {
   var code = req.query.code;
 
-  request.post('https://graph.facebook.com/v2.3/oauth/access_token?client_id=523614004477797&redirect_uri=http://procrastinationation.mybluemix.net/facebook&client_secret=9fb99aa844667e815c990b41aa086d27&code=' + code, function(err, res) {
-    var access_token = res.body.access_token;
-      request.post('https://graph.facebook.com/me?access_token=' + access_token, function(err, res) {
-        console.log(res.body);
+
+  request.post('https://graph.facebook.com/v2.3/oauth/access_token?client_id=523614004477797&redirect_uri=http://procrastinationation.mybluemix.net/facebook&client_secret=9fb99aa844667e815c990b41aa086d27&code=' + code, function(err, res1) {
+    var access_token = res1.body.access_token;
+
+      // Validate the token and its permissions
+      request.get('https://graph.facebook.com/me/permissions?access_token=' + access_token, function(err, res2) {
+        var permissions = res2.body.data;
+
+        var isGood = false;
+
+        var userFriends = 'user_friends';
+
+        for (var i = 0; i < permissions.length; i++) {
+          if (permissions[i].permission === userFriends) {
+            if (permissions[i].status === 'granted') {
+              isGood = true;
+            }
+            break;
+          }
+        }
+
+        if (isGood) {
+          res.send({
+            access_token: access_token
+          });
+        } else {
+          res.redirect('https://www.facebook.com/dialog/oauth?client_id=523614004477797&redirect_uri=http://procrastinationation.mybluemix.net/facebook&auth_type=rerequest&scope=' + userFriends);
+        }
       });
   });
 });
